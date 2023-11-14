dc4d9e348ba2f95ce8a8b75b21e54586a9db45fb
JoshuaIsADev <JoshuaIsADev@gmail.com>
2023-11-13 17:09:49 -0800

    under maintenance. created maintenance page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import './styles.css';
-import Header from './Header';
-import Navigation from './Navigation';
-import About from './About';
-import Writer from './Writer';
-import Ux from './Ux';
-import Footer from './Footer';
+import Maintenance from './Maintenance';
+// import Header from './Header';
+// import Navigation from './Navigation';
+// import About from './About';
+// import Writer from './Writer';
+// import Ux from './Ux';
+// import Footer from './Footer';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Navigation />
-      <main>
-        <About />
-        <Writer />
-        <Ux />
-      </main>
-      <Footer />
-    </div>
+    // <div>
+    //   <Header />
+    //   <Navigation />
+    //   <main>
+    //     <About />
+    //     <Writer />
+    //     <Ux />
+    //   </main>
+    //   <Footer />
+    // </div>
+    <Maintenance />
   );
 }
 
